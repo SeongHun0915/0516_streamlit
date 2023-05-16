96be9015628da84e3b56fe587c20c96f2bbb3333
SeongHun0915 <126744004+SeongHun0915@users.noreply.github.com>
2023-05-16 11:34:10 +0900

    Update app.py
    to test another source code

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-import streamlit as st
+import streamlit st
+import matplotlib.pyplot as plt
+import numpy as np
 
-x = st.slider('Select a value')
-st.write(x, 'squared is ', x * x)
+rand = np.random.normal(1, 2, size = 20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
+
+
+
+#import streamlit as st
+
+#x = st.slider('Select a value')
+#st.write(x, 'squared is ', x * x)
 
 #콤마는 코드를 이어지게 하기위한.
